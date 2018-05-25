c9db4b9d6998811ec7b4804d9c8ea6263434be60
Dustin Myers <dustinmyers@users.noreply.github.com>
2018-05-25 11:01:18 -0600

    Update usersRoutes.js

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -3,86 +3,85 @@ const db = require('../data/helpers/userDb');
 
 const router = express.Router();
 
-//// GET
-router.get('/', (req, res) => {
+router.get('/', (req, res) => {  // GET all users
   db.get()
     .then(response => {
-      res.json(response);
+      res.send(response);
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500);
       console.error(err);
-    });
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {  // GET user by id
   db.get(req.params.id)
-    .then(users => {
-      if (users.length === 0) {
-        res.status(404).json({ message: 'user not found' });
+    .then(response => {
+      console.log(response);
+      if (response) {
+        res.json(response);
       } else {
-        res.json(users[0]);
+        res.status(400);
+        res.json({ error: `No user found with id: ${req.params.id}` });
       }
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500);
       console.error(err);
-    });
+    })
 });
 
-//// POST
-router.post('/', (req, res, next) => {
+router.get('/:id/posts', (req, res) => {
+  db.getUserPosts(req.params.id)
+    .then(response => {
+      res.send(response);
+    }).catch(err => {
+    res.status(500);
+    console.error(err);
+  })
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
   const userInformation = req.body;
 
-  db
-    .insert(userInformation)
+  db.insert(userInformation)
     .then(response => {
-      res.status(201).json(response);
-    })
-    .catch(err => {
-      res.json({ error: err });
-      console.log(err);
-    });
+      res.status(201);
+      res.json(response);
+    }).catch(err => {
+      res.status(500);
+      console.error(err);
+  })
 });
 
-//// PUT
-router.put('/:id', function(req, res) {
-  const { id } = req.params;
-  const update = req.body;
 
-  db
-    .update(id, update)
-    .then(count => {
-      if (count > 0) {
-        db.get(id).then(users => {
-          res.status(200).json(users[0]);
-        });
+router.put('/:id', (req, res) => {
+  db.update(req.params.id, req.body)
+    .then(response => {
+      if (response === 1) {
+        db.get(req.params.id).then(updatedUser => {
+          res.json(updatedUser);
+        })
       } else {
-        res.status(404).json({ msg: 'user not found' });
+        res.status(500);
+        res.json({ error: `user with id ${id} was not updated` });
       }
-    })
-    .catch(err => {
-      res.status(500).json(err);
+    }).catch(err => {
+      res.status(500);
       console.error(err);
-    });
+    })
 });
 
-//// DELETE
-router.delete('/', function(req, res) {
-  const { id } = req.query;
-  let user;
-  db.get(id)
-    .then(foundUser => {
-      user = { ...foundUser[0] };
-
-      db.remove(id).then(response => {
-        res.status(200).json(user);
-      });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err });
-      console.error(err);
-    });
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(response => {
+      res.json(response)
+    }).catch(err => {
+    res.status(500);
+    console.error(err);
+  });
 });
+
 
 module.exports = router;
